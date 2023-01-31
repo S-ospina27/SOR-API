043d5f805f47a24f6bcf98502aa5d4dbeffbed7c
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-01-31 10:35:57 -0500

    feat: added response

--- public/index.php
+++ public/index.php
@@ -6,11 +6,13 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 (Dotenv\Dotenv::createImmutable(__DIR__ . "/../"))->load();
 
 // encabezados
+header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json");
-
+date_default_timezone_set("America/bogota");
 // constantes
 define('env', (object) $_ENV);
 define('request', LionRequest\Request::capture());
+define('response',LionRequest\Response::getInstance());
 
 // inicializar base de datos
 use LionSQL\Drivers\MySQL as DB;
